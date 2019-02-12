29f1695c398ed2559d7e92d2323411ccb9ffc86c
kuoruan <kuoruan@gmail.com>
2019-02-12 10:54:17 +0800

    ovz-bbr: Update copyright year
    Signed-off-by: kuoruan <kuoruan@gmail.com>

--- ovz-bbr/ovz-bbr-installer.sh
+++ ovz-bbr/ovz-bbr-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 : <<-'EOF'
-Copyright 2017 Xingwang Liao <kuoruan@gmail.com>
+Copyright 2017-2019 Xingwang Liao <kuoruan@gmail.com>
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
