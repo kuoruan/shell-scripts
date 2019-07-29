be86c30d2e0d8d9792cef63ee85c78e13a2937ed
Xingwang Liao <kuoruan@gmail.com>
2019-07-29 17:56:43 +0800

    chore(kcptun): bump to v25

--- kcptun/kcptun.sh
+++ kcptun/kcptun.sh
@@ -17,7 +17,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # 版本信息，请勿修改
 # =================
-SHELL_VERSION=24
+SHELL_VERSION=25
 CONFIG_VERSION=6
 INIT_VERSION=3
 # =================
