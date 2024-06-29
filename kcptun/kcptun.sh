a128392efe5952df690651d8b1cf807c5a22543f
tuuz <silverhawk@foxmail.com>
2024-06-29 13:09:26 +0800

    新增SMUXVer和STEAMBUF新功能

--- kcptun/kcptun.sh
+++ kcptun/kcptun.sh
@@ -68,9 +68,13 @@ D_NODELAY=1
 D_INTERVAL=20
 D_RESEND=2
 D_NC=1
-D_SOCKBUF=4194304
-D_SMUXBUF=4194304
+D_SOCKBUF=16777217
+D_SMUXBUF=16777217
 D_KEEPALIVE=10
+
+# KCP新版支持
+D_SMUXVER=1
+D_STREAMBUF=4194304
 # ======================
 
 # 当前选择的实例 ID
@@ -1701,6 +1705,8 @@ set_kcptun_config() {
 		sockbuf=""
 		smuxbuf=""
 		keepalive=""
+		streambuf=""
+		smuxver=""
 		cat >&1 <<-EOF
 		---------------------------
 		不配置隐藏参数
@@ -1979,6 +1985,54 @@ set_hidden_parameters() {
 	cat >&1 <<-EOF
 	---------------------------
 	smuxbuf = ${smuxbuf}
+	---------------------------
+	EOF
+
+	[ -z "$keepalive" ] && keepalive="$D_KEEPALIVE"
+	while true
+	do
+		cat >&1 <<-'EOF'
+		请设置 Keepalive 的间隔时间
+		EOF
+		read -p "(单位: s, 默认值: ${keepalive}, 前值: 5): " input
+		if [ -n "$input" ]; then
+			if ! is_number "$input" || [ $input -le 0 ]; then
+				echo "输入有误, 请输入大于0的数字!"
+				continue
+			fi
+
+			keepalive=$input
+		fi
+		break
+	done
+
+	cat >&1 <<-EOF
+	---------------------------
+	keepalive = ${keepalive}
+	---------------------------
+	EOF
+
+	[ -z "$smuxver" ] && smuxver="$D_SMUXVER"
+	while true
+	do
+		cat >&1 <<-'EOF'
+		请设置 smux 的版本
+		EOF
+		read -p "(1,2, 默认值: ${smuxver}, 老版本默认1，新版本默认2): " input
+		if [ -n "$input" ]; then
+			if ! is_number "$input" || [ $input -le 0 ]; then
+				echo "输入有误, 请输入大于0的数字!"
+				continue
+			fi
+
+			smuxver=$input
+		fi
+		break
+	done
+
+	cat >&1 <<-EOF
+	---------------------------
+	keepalive = ${keepalive}
 	---------------------------
 	EOF
 
@@ -2331,7 +2385,7 @@ show_current_instance_info() {
 
 	show_configs "key" "crypt" "mode" "mtu" "sndwnd" "rcvwnd" "datashard" \
 		"parityshard" "dscp" "nocomp" "quiet" "tcp" "nodelay" "interval" "resend" \
-		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive"
+		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive" "streambuf" "smuxver"
 
 	show_version_and_client_url
 
@@ -2373,7 +2427,7 @@ show_current_instance_info() {
 
 	gen_client_configs "crypt" "mode" "mtu" "sndwnd" "rcvwnd" "datashard" \
 		"parityshard" "dscp" "nocomp" "quiet" "tcp" "nodelay" "interval" "resend" \
-		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive"
+		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive" "streambuf" "smuxver"
 
 	cat >&1 <<-EOF
 
@@ -2407,7 +2461,7 @@ show_current_instance_info() {
 
 	gen_mobile_configs "crypt" "mode" "mtu" "sndwnd" "rcvwnd" "datashard" \
 		"parityshard" "dscp" "nocomp" "quiet" "tcp" "nodelay" "interval" "resend" \
-		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive"
+		"nc" "acknodelay" "sockbuf" "smuxbuf" "keepalive" "streambuf" "smuxver"
 
 	cat >&1 <<-EOF
 
